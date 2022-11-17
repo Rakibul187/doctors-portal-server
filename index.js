79b445eaa95b98caeedef57f507650e27f8d6649
Rakibul Islam <rakibul241221@gmail.com>
2022-11-17 20:37:05 +0600

    aggregate, filter, map

--- index.js
+++ index.js
@@ -1,11 +1,126 @@
 const express = require('express');
-const cors = require('cors');
 const app = express()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors');
+require('dotenv').config()
+
 const port = process.eventNames.PORT || 5000;
 
 // middleware
 app.use(cors())
 app.use(express.json())
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.farjvzi.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+
+    try {
+        const appointmentOptionCollection = client.db("doctorsPortal").collection("appointmentOptions");
+        const bookingsCollection = client.db("doctorsPortal").collection("bookings");
+
+        app.get('/appoinmentOptions', async (req, res) => {
+            const date = req.query.date;
+            console.log(date)
+            const query = {}
+            const options = await appointmentOptionCollection.find(query).toArray()
+            const bookingQuery = { appointmentDate: date }
+            const alreadyBooked = await bookingsCollection.find(bookingQuery).toArray()
+
+            options.forEach(option => {
+                const optionBooked = alreadyBooked.filter(book => book.treatment === option.name)
+                const bookedSlots = optionBooked.map(option => option.slot)
+                const remainingSlots = option.slots.filter(slot => !bookedSlots.includes(slot))
+                option.slots = remainingSlots
+            })
+            res.send(options)
+        })
+
+        // agregate
+        app.get('/v2/appointmentOptions', async (req, res) => {
+            const date = req.query.date;
+            const options = await appointmentOptionCollection.aggregate([
+                {
+                    $lookup: {
+                        from: 'bookings',
+                        localField: 'name',
+                        foreignField: 'treatment',
+                        pipeline: [
+                            {
+                                $match: {
+                                    $expr: {
+                                        $eq: ['$appointmentDate', date]
+                                    }
+                                }
+                            }
+                        ],
+                        as: 'booked'
+                    }
+                },
+                {
+                    $project: {
+                        name: 1,
+                        slots: 1,
+                        booked: {
+                            $map: {
+                                input: '$booked',
+                                as: 'book',
+                                in: '$$book.slot'
+                            }
+                        }
+                    }
+                },
+                {
+                    $project: {
+                        name: 1,
+                        slots: {
+                            $setDifference: ['$slots', '$booked']
+                        }
+                    }
+                }
+            ]).toArray();
+            res.send(options);
+        })
+
+
+        /* 
+         api namning convention
+         ========================
+        1.app.get('/bookings')
+        2. app.get('/bookings/:id')
+        3. app.post('/bookings')
+        4. app.path('/bookings/:id')
+        5. app.delete('/bookings/:id')        
+        */
+        // booking
+        app.post('/bookings', async (req, res) => {
+            const booking = req.body
+            console.log(booking)
+            const query = {
+                appointmentDate: booking.appointmentDate,
+                treatment: booking.treatment,
+                email: booking.email
+            }
+            const alreadyBooked = await bookingsCollection.find(query).toArray()
+
+            if (alreadyBooked.length) {
+                const message = `You already have a booking on ${booking.appointmentDate}`
+                return res.send({ acknowledged: false, message })
+            }
+
+            const result = await bookingsCollection.insertOne(booking)
+            res.send(result)
+        })
+    }
+    finally {
+
+    }
+
+}
+run().catch(e => console.log(e))
+
 
 app.get('/', async (req, res) => {
     res.send('Doctors Portal Server is Running')
